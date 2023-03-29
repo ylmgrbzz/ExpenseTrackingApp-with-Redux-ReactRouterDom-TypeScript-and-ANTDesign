750e96f95dc6b8c93f60f8c907892bfe5c0664f4
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-29 17:45:13 +0300

    ExpenseTrackinkApp

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
+import "antd/dist/reset.css";
+import rootReducer from "./store";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
-root.render(<App />);
---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { combineReducers } from "redux";
+
+
+interface AppState {
+    user: any;
+    categories: any;
+    records: any;
+}
+
+const rootReducer = combineReducers<AppState>({
+    user: () => { },
+    categories: () => { },
+    records: () => { },
+});
+
+
+export default rootReducer;
