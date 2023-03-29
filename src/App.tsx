212d32095ad09d9ca8fdf08d00ebcc93cd1e3983
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-29 18:48:19 +0300

    ExpenseTrackinkApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import SignUp from "./components/SignUp";
+import { Routes, Route } from "react-router-dom";
+import { Layout, Menu } from "antd";
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
-  return <div className="App">ylm</div>;
+  return (
+    <Layout>
+      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content
+        className="site-layout"
+        style={{ padding: "50px", marginTop: 64 }}
+      >
+        <Routes>
+          <Route path="/register" element={<SignUp />} />
+        </Routes>
+
+        {/* <Route path="/login" component={Login} />
+        <PrivateRoute path="/categories" component={Categories} />
+        <PrivateRoute path="/records" component={Records} /> */}
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Expense Tracker @ React Dersleri 2021
+      </Footer>
+    </Layout>
+  );
 }
 
 export default App;
+
+{
+  /* <Routes>
+  <Route path="/register" element={<SignUp />} />
+</Routes>; */
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "antd/dist/reset.css";
 import rootReducer from "./store";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-ReactDOM.render(
+let store = createStore(rootReducer, applyMiddleware(thunk));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
